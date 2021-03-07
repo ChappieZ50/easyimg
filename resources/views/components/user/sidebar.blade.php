8215f56609549953d3e684c8353d25023695226c
Uğur <ugurtosun50@gmail.com>
2021-03-07 23:42:38 +0300

    Admin Panel link added

--- resources/views/components/user/sidebar.blade.php
+++ resources/views/components/user/sidebar.blade.php
@@ -1,5 +1,8 @@
 <div class="irob-sidebar col-xl-2 col-lg-2 col-md-12 col-sm-12">
     <ul>
+        @if(auth()->user()->is_admin)
+            <li><a href="{{route('admin.home')}}">Admin Panel</a></li>
+        @endif
         <li class="{{request()->routeIs('user.profile') ? 'active' : ''}}"><a href="{{route('user.profile')}}">Profile</a></li>
         <li class="{{request()->routeIs('user.images') ? 'active' : ''}}"><a href="{{route('user.images')}}">My Images</a></li>
         <li><a href="{{route('home')}}">Upload Image</a></li>
