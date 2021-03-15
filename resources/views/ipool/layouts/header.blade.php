6371ff8ac6f5b852d9adb559bb1492e61f44076f
Uğur <ugurtosun50@gmail.com>
2021-03-16 00:06:20 +0300

    Burger menu fixed and some responsive bugs fixed.

--- resources/views/ipool/layouts/header.blade.php
+++ resources/views/ipool/layouts/header.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-    <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
+    <div class="text-center navbar-brand-wrapper d-none d-lg-flex align-items-top justify-content-center">
         <a class="navbar-brand brand-logo" href="{{route('admin.home')}}">
             <img src="{{get_logo()}}" alt="logo" style="width: 140px;"/> </a>
         <a class="navbar-brand brand-logo-mini" href="{{route('admin.home')}}">
             <img src="{{get_logo()}}" alt="logo" style="width: 100%;"/> </a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown d-none d-xl-inline-block user-dropdown">
+        <ul class="navbar-nav ml-lg-auto">
+            <li class="nav-item dropdown d-inline-block user-dropdown">
                 <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
                     <img class="img-xs rounded-circle" src="{{avatar_url(auth()->user()->avatar)}}" alt="{{auth()->user()->username}}"> </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown ipool-admin-menu" aria-labelledby="UserDropdown">
@@ -35,7 +35,7 @@
                 </div>
             </li>
         </ul>
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center ml-auto" type="button" data-toggle="offcanvas">
             <span class="mdi mdi-menu"></span>
         </button>
     </div>
